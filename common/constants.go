46841f8c2a5a8f512b0bb8882813c233f21a8e66
Aarti Jivrajani <aartij17@gmail.com>
2020-05-19 09:14:54 -0700

    add deployment stages constants

--- common/constants.go
+++ common/constants.go
@@ -3,4 +3,17 @@ package common
 const (
 	TRACE_ID         = "TRACE_ID"
 	GIT_BUILD_FOLDER = "build/"
+
+	// STAGE PRINT
+	STAGE_FORMAT      = "[%s]: %s" // use this as fmt.Sprintf(STAGE_FORMAT, STAGE_STATUS_WIP, STAGE_GIT)
+	STAGE_STATUS_WIP  = "IN PROGRESS"
+	STAGE_STATUS_DONE = "COMPLETED"
+
+	// STAGES
+	STAGE_GIT                   = "Git Repo Cloned"
+	STAGE_COLLECTING_DOCKER_REQ = "Collecting build requirements"
+	STAGE_BUILDING_DOCKER_IMAGE = "Building docker image in progress"
+	STAGE_DOCKER_IMAGE_BUILT    = "Docker image built"
+
+	STAGE_CREATING_PODS = "Creating Kubernetes Artefacts"
 )
