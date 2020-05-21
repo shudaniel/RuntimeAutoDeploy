de6dc2913182e7da4432fa6387bc6da7a01ad4bf
Aarti Jivrajani <aartij17@gmail.com>
2020-05-20 22:30:51 -0700

    run main routine and wait/block on a signal channel

--- trigger/main.go
+++ trigger/main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"RuntimeAutoDeploy/common"
 	"RuntimeAutoDeploy/trigger/handlers"
 	"net/http"
+	"os"
+	"os/signal"
 
 	log "github.com/Sirupsen/logrus"
 )
 
 func main() {
+	var err error
 	log.Info("RAD Service Started")
-	err := handlers.Cleanup(common.GIT_BUILD_FOLDER)
-	if err != nil {
-		log.Error("Exiting now.")
-		return
-	}
-	go handlers.StartStatusService()
+	//err := handlers.Cleanup(common.GIT_BUILD_FOLDER)
+	//if err != nil {
+	//	log.WithFields(log.Fields{
+	//		"error": err.Error(),
+	//	}).Error("error cleaning up")
+	//	return
+	//}
+	handlers.StartStatusService()
 
 	http.HandleFunc("/trigger", handlers.RADTriggerHandler)
+	http.HandleFunc("/status", handlers.RADStatusHandler)
+
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		for _ = range signalChan {
+			log.Info("Received an interrupt, stopping all connections...")
+			//cancel()
+			cleanupDone <- true
+		}
+	}()
+	<-cleanupDone
 }
