79ea483e31e195c6cc702366304a9231909c1c54
Aarti Jivrajani <aartij17@gmail.com>
2020-05-20 22:29:41 -0700

    add status handler

--- trigger/handlers/status.go
+++ trigger/handlers/status.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"RuntimeAutoDeploy/common"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
 	"gopkg.in/redis.v5"
 )
 
@@ -11,6 +15,37 @@ var (
 type Status struct {
 	redisConn  *redis.Client
 	statusList []string
+}
+
+func RADStatusHandler(w http.ResponseWriter, r *http.Request) {
+	// When the user pings this, return the status
+	var (
+		err        error
+		statusList []string
+	)
+	if r.Method != "GET" {
+		log.Error("error. Received incorrect HTTP method. Expecting GET")
+		return
+	}
+	keys, OK := r.URL.Query()[common.TRACE_ID]
+	if !OK || len(keys[0]) < 1 {
+		log.Println("Url Param 'TRACE_ID' is missing")
+		return
+	}
+	traceId := r.FormValue(common.TRACE_ID)
+
+	// Fetch the status list from REDIS
+	statusList, err = statusRoutine.getStatusList(traceId)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte("internal error, please try again"))
+		return
+	}
+
+	for i := range statusList {
+		_, _ = w.Write([]byte(statusList[i]))
+	}
+	return
 }
 
 func StartStatusService() {
