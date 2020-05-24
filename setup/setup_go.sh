eb81b17f776d559be204071460e1cd0703dcca12
Aarti Jivrajani <aartij17@gmail.com>
2020-05-24 14:44:15 -0700

    add script to install go, dependencies, and redis

--- setup/setup_go.sh
+++ setup/setup_go.sh
@@ -1,14 +1,33 @@
+sudo su
 curl -O https://dl.google.com/go/go1.14.3.linux-amd64.tar.gz
-
 tar xvf go1.14.3.linux-amd64.tar.gz
-
 sudo chown -R root:root ./go
 sudo mv go /usr/local
-
-export GOPATH=$HOME/rad
+export GOPATH=$HOME/work
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+mkdir -p $HOME/work/src
+cd $HOME/work/src
 
-cd /usr/local/go
+# get all the dependencies
+
+go get github.com/docker/docker/api/types
+go get github.com/docker/docker/client
+go get github.com/go-git/go-git
+go get github.com/google/uuid
+go get github.com/sirupsen/logrus
+go get gopkg.in/redis.v5
+go get k8s.io/api/apps/v1
+go get k8s.io/api/core/v1
+go get k8s.io/apimachinery/pkg/api/resource
+go get k8s.io/apimachinery/pkg/apis/meta/v1
+go get k8s.io/client-go/kubernetes
+go get k8s.io/client-go/tools/clientcmd
+go get k8s.io/client-go/util/homedir
+
+# get the user repository
 git clone https://github.com/shudaniel/RuntimeAutoDeploy.git
-cd RuntimeAutoDeploy
 
+# redis
+
+sudo apt update
+sudo apt install redis-server -y
