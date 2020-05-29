1b684885c144bdc4f36c10abffe3d7dbf6e6a3ff
Daniel Shu <danielshu98@gmail.com>
2020-05-29 00:51:20 -0700

    fixed bug in throughput test

--- performance_tests/throughput_tests.py
+++ performance_tests/throughput_tests.py
@@ -1,17 +1,12 @@
-# importing the requests library 
-import requests 
-  
-# api-endpoint 
-URL = "localhost:8080/trigger"
-git_repo = "https://github.com/AartiJivrajani/Auto-Pub-Sub.git"  
-  
-# defining a params dict for the parameters to be sent to the API 
-PARAMS = {'gitrepo': git_repo} 
-  
-# sending get request and saving the response as response object 
-r = requests.post(url = URL, params = PARAMS) 
-  
-# extracting data in json format 
-data = r.test
-  
-print(data)
+import requests
+
+url = "http://localhost:8080/trigger"
+
+payload = "{\n\t\"gitrepo\": \"https://github.com/AartiJivrajani/Auto-Pub-Sub.git\"\n}"
+headers = {
+    'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
